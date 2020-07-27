a71f69bc44a9eb472da6d38ad8803652db34e567
melaniede <debarrosmelanie@gmail.com>
2020-07-26 22:36:34 -0400

    change clone of rows to creating rows in js with hr block populated

--- assets/script.js
+++ assets/script.js
@@ -4,19 +4,37 @@ $( document ).ready(function() {
     console.log(currentTime);
     
     let row = $(".row");
+    //let date = moment(date).hour(8);
+    let hour = moment(currentTime).hour(9); 
+    console.log(hour);
 
     function createSchedule (){
 
-        
-        for (let i = 0; i < 11; i++) {
-            $(".container").append($("#rows").clone().removeAttr('id'));
-            console.log(i);
+        //row.text($("#hour").text(hour.format("H a")));
+        for (let i = 0; i < 12; i++) {
+            // Bootstrap row
+            const row = $("<div>").addClass("row"); 
+            
+            //Add row container
+            $(".container").append(row);
+
+            // Display hour block
+            row.append($("<div>").addClass("col-2 bg-primary").text(hour.format("H a")));
+            
+            // Display input block
+            row.append($("<textarea>").addClass("col-8 bg-secondary"));
+            
+            // Display save button 
+            row.append($("<button>").addClass("col-2 btn bg-info far fa-save"));
+
+            // Add an hour to hour block
+            hour.add(1, "hour");
+
 
             
 
         }
     }
     createSchedule();
-   // $( window ).on("load", createSchedule());
 
 });
