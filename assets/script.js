0c3b6144c2853795700716da0b5dcde61ab420f0
melaniede <debarrosmelanie@gmail.com>
2020-07-26 20:56:55 -0400

    build the rest of the rows from js

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,22 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    const currentTime = moment();
+    console.log(currentTime);
     
+    let row = $(".row");
+
+    function createSchedule (){
+
+        
+        for (let i = 0; i < 11; i++) {
+            $(".container").append($("#rows").clone().removeAttr('id'));
+            console.log(i);
+
+            
+
+        }
+    }
+    createSchedule();
+   // $( window ).on("load", createSchedule());
+
 });
